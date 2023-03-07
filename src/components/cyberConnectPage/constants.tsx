ede93be07f6e8761737c125569d9160155a17e36
emerson <emerson@dappio.xyz>
2023-03-07 23:13:47 +0800

    Add constants

--- src/components/cyberConnectPage/constants.tsx
+++ src/components/cyberConnectPage/constants.tsx
@@ -1,4 +1,10 @@
+export const DOMAIN = "daisi.social";
 export const DAISI_DB_ENDPOINT = "https://localhost:8000/";
 export const ARWEAVE_ENDPOINT = "https://arweave.net/";
 export const CYBERCONNECT_ENDPOINT = "https://api.cyberconnect.dev/testnet/";
+export const IPFS_GATEWAY = "https://daisi.infura-ipfs.io";
+
+// TODO: deprecate and move to .env
 export const X_API_KEY = "Cpdjzg8Yv91z5ds7EYFmN8pXQJfMzX4u";
+export const PROJECT_ID = "2MKOnm6IhNBqLYJIAx1tVNrrP3G";
+export const API_KEY = "2a192d1d29c89da90a77ad923b813718";
